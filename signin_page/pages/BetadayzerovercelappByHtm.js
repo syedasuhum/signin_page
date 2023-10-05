42b4f846ef14736777c58ee5bf509e72dc767bac
syedasuhum <112943478+syedasuhum@users.noreply.github.com>
2023-10-05 20:19:00 +0530

    Add files via upload

---
+++ signin_page/pages/BetadayzerovercelappByHtm.js
@@ -0,0 +1,18 @@
+import DivlayoutAuthPage from "../components/DivlayoutAuthPage";
+import "./BetadayzerovercelappByHtm.css";
+const BetadayzerovercelappByHtm = () => {
+  return (
+    <div className="betadayzerovercelapp-by-htm">
+      <DivlayoutAuthPage />
+      {/* <div className="button">
+        <div className="feedback">Feedback</div>
+        <div className="div-hj-t5uly-minimizedwidget">
+          <div className="symbol"></div>
+          <div className="symbol1"></div>
+        </div>
+      </div> */}
+    </div>
+  );
+};
+
+export default BetadayzerovercelappByHtm;
